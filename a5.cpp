269995ca9ec4ac9cec6203f44e47b4643f58c987
brolyRough <43203261+brolyRough@users.noreply.github.com>
2018-09-12 15:05:00 +0530

    Add files via upload

---
+++ a5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Pair{
+public:
+	int value;
+	int distance;
+};
+
+class Graph {
+public:
+	vector <Pair>* adj;
+	vector <int>* burnt;
+	vector <int>* expectedBurnt;
+
+
+
+};
+
+int main(){
+cout << "Teja " << endl;
+
+}
